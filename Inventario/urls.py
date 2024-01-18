180bd740382aafa05b1af1088a0f3d78496e33a8
lreyesp26 <lreyesp@uteq.edu.ec>
2024-01-17 22:11:55 -0500

    Proveedores añadido + Inventario funcional

--- Inventario/urls.py
+++ Inventario/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('crearinventario/', CrearInventario.as_view(), name='crearinventario'),
     path('verinventario/', ListarInventario.as_view(), name='verinventario'),
+    path('editar/<int:id_inventario>/', EditarInventario.as_view(), name='editar_inventario'),
+
 ]
