a925c2c039dab5506733b4a1cf6da6185b6e74ce
RafaelNavas03 <84700397+RafaelNavas03@users.noreply.github.com>
2024-01-30 21:59:35 -0500

    Reserva bak actualizado 1

--- Mesa/urls.py
+++ Mesa/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('ver_mesas/', MostrarMesas.as_view(), name='MostrarMesas'),
     path('editar_mesa/<int:id_mesa>/', EditarMesa.as_view(), name='editar_mesa'),
     path('crear_reservacion/', CrearReservacion.as_view(), name='crearreservacion'),
+    path('listar_reservaciones/', ListarReservaciones.as_view(), name='listar_reservaciones'),
+    path('editar_reservacion/<int:id_reservacion>/', EditarReservacion.as_view(), name='editar_reservacion'),
 ]
